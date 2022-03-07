9b0d0ce9c3f974bae24d9b2b892819f55d54e5a5
nk1433 <85053086+nk1433@users.noreply.github.com>
2022-03-07 15:54:31 +0530

    Created a document and pushed to db through Model.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
+/* eslint-disable no-console */
 const express = require('express');
-const { connectDb } = require('./lib/mongooseManager');
+const {
+	connectDb, createSchema, createModelInstance,
+	createModel,
+} = require('./lib/mongooseManager');
 const { getData } = require('./lib/helpers');
 
 const app = express();
 const { DATABASE, PORT } = getData();
 
 connectDb(DATABASE);
+
+const tourSpec = {
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	rating: {
+		type: Number,
+		default: 6,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+};
+
+const tourSchema = createSchema(tourSpec);
+const Tour = createModel('Tour', tourSchema);
+
+const testTour = createModelInstance(Tour, {
+	name: 'Manali',
+	rating: 9,
+	price: 25000,
+});
+
+// TODO: unable to destructure save.
+testTour.save().then((doc) => console.log(doc))
+	.catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
 	res.send('hello');
